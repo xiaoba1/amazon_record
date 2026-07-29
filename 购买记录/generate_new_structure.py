43806cc5fe322f55853fe76f24f44b16fea96361
xiaoba1 <xiaoba1@example.com>
2026-07-29 08:58:03 +0000

    feat: 新增销售订单249-3150938-6827810 切蒜器卖出第2个

--- 购买记录/generate_new_structure.py
+++ 购买记录/generate_new_structure.py
@@ -695,6 +695,21 @@ SALES_DATA = [
         "order": "250-31255524-9007023",
         "remark": "",
     },
+    {
+        "platform": "Amazon日本站",
+        "order_date": datetime(2026, 7, 29),
+        "ship_date": datetime(2026, 7, 31),
+        "product": "切蒜器带透明收纳容器",
+        "spec": "绿色",
+        "sku": "ba0010",
+        "qty": 1,
+        "price": 699,
+        "tax": 64,
+        "fee": 169,
+        "revenue": 929,
+        "order": "249-3150938-6827810",
+        "remark": "",
+    },
 ]
 
 
